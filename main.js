c045a03cf31615e8613564da598a7b5b8f2ad8d6
sorawit sirimaleewattana <sorawitsirimaleewattana@sorawits-MacBook-Pro.local>
2021-01-18 00:45:41 +0700

    It can print thai language

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 console.log("STEP #1")
 const escpos = require('escpos');
+const iconv  = require('iconv').Iconv;
+const path = require('path');
 console.log("STEP #2")
 
 // install escpos-usb adapter module manually
@@ -10,31 +12,71 @@ const device  = new escpos.USB();
 // const device  = new escpos.Network('localhost');
 // const device  = new escpos.Serial('/dev/usb/lp0');
 
-const options = { encoding: "GB18030" /* default */ }
+// const options = { encoding: "CP874" }
 // encoding is optional
 
-const printer = new escpos.Printer(device, options);
+const printer = new escpos.Printer(device);
 
-device.open(function(error){
+// const logo_img = path.join(__dirname, 'logo.png');
+// console.log("logo_img", logo_img);
+
+device.open(function(error) {
   printer
+  .encode('tis-620')
   .font('a')
-  .align('ct')
-  .style('bu')
+  .align("ct")
   .size(1, 1)
-  .text('The quick brown fox jumps over the lazy dog')
-  .text('敏捷的棕色狐狸跳过懒狗')
-  .barcode('1234567', 'EAN8')
-  .table(["One", "Two", "Three"])
-  .tableCustom(
-    [
-      { text:"Left", align:"LEFT", width:0.33, style: 'B' },
-      { text:"Center", align:"CENTER", width:0.33},
-      { text:"Right", align:"RIGHT", width:0.33 }
-    ],
-    { encoding: 'cp857', size: [1, 1] } // Optional
-  )
-  .qrimage('https://github.com/song940/node-escpos', function(err){
+  .text('Datability Co., Ltd')
+  .font('a')
+  .size(1, 1)
+  .text('บริษัท ดาต้าบิลิตี้ จำกัด'.toString('hex'))
+
+  .barcode('123456789123', 'EAN13')
+
+  .qrimage('http://datability.info', function(err){
+    this.style('bu')
+    this.size(1, 1)
+    this.font('b')
+    this.text('http://datability.info')
+
+    this.feed("lf")
     this.cut();
     this.close();
   });
+  // .feed("lf")
+  // .cut()
+  // .close(); 
+
 });
+
+// 'use strict';
+// // const escpos = require('../');
+// const escpos = require('escpos');
+// escpos.USB = require('escpos-usb');
+// // const device  = new escpos.USB(0x0485, 0x7541);
+// const device  = new escpos.USB();
+// // const device  = new escpos.Network('localhost');
+// // const device  = new escpos.Serial('/dev/usb/lp0');
+// const printer = new escpos.Printer(device);
+
+
+// device.open(function(err){
+//   printer
+//   .model('qsprinter')
+//   .font('a')
+//   .align('ct')
+//   .style('bu')
+//   .size(1, 1)
+//   .encode('tis620')
+//   .text('The quick brown fox jumps over the lazy dog')
+//   .text('สวัสดีภาษาไทย')
+//   .cut()
+//   .close();
+//   // .text('敏捷的棕色狐狸跳过懒狗')
+//   // .barcode('1234567', 'EAN8')
+//   // .qrimage('https://github.com/song940/node-escpos', function(err){
+//   //   this.cut();
+//   //   this.close();
+//   // });
+
+// });
